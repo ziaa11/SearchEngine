b671d0cb2349e9846a6f6e97f527a2c0f564e6ee
Zia Ur Rehman <ziaprivate11@gmail.com>
2023-03-23 14:41:27 +0530

    Updated boolean.py

--- boolean.py
+++ boolean.py
@@ -25,30 +25,26 @@ def boolean_and(postings1, postings2):
         else:
             j += 1
     return result
-    # print(inverted_index[postings1])
-    # print(inverted_index[postings2])
 
-# print(len(inverted_index["manhattan"]))
 
-# result = boolean_and("manhattan", "new")
-# print(result)
-
-# def boolean_or(postings1, postings2):
-#     result = []
-#     i = j = 0
-#     while i < len(postings1) and j < len(postings2):
-#         if postings1[i] == postings2[j]:
-#             result.append(postings1[i])
-#             i += 1
-#             j += 1
-#         elif postings1[i] < postings2[j]:
-#             result.append(postings1[i])
-#             i += 1
-#         else:
-#             result.append(postings2[j])
-#             j += 1
-#     result += postings1[i:] + postings2[j:]
-#     return result
+def boolean_or(postings1, postings2):
+    result = []
+    i = j = 0
+    while i < len(inverted_index[postings1]) and j < len(inverted_index[postings2]):
+        my_list_1 = inverted_index[postings1] #posting list 1
+        my_list_2 = inverted_index[postings2] #posting list 2
+        if my_list_1[i] == my_list_2[j]:
+            result.append(my_list_1[i])
+            i += 1
+            j += 1
+        elif my_list_1[i] < my_list_2[j]:
+            result.append(my_list_1[i])
+            i += 1
+        else:
+            result.append(my_list_2[j])
+            j += 1
+    result += my_list_1[i:] + my_list_2[j:]
+    return result
 
 # def boolean_not(postings, num_docs):
 #     all_docs = set(range(1, num_docs+1))
@@ -56,53 +52,7 @@ def boolean_and(postings1, postings2):
 #     not_posting = sorted(list(all_docs - posting_set))
 #     return not_posting
 
-# def process_query(query, inverted_index):
-#     stack = []
-#     tokens = query.split()
-#     flag=0
-#     for token in tokens:
-#         if flag==1: continue
-#         if token == "AND":
-#             operand2 = stack.pop()
-#             operand1 = stack.pop()
-#             result = boolean_and(operand1, operand2)
-#             stack.append(result)
-#         # elif token == "OR":
-#         #     operand2 = stack.pop()
-#         #     operand1 = stack.pop()
-#         #     result = boolean_or(operand1, operand2)
-#         #     stack.append(result)
-#         # elif token == "NOT":
-#         #     operand = stack.pop()
-#         #     result = boolean_not(operand, num_docs=len(inverted_index["manhattan"]))
-#         #     stack.append(result)
-#         else:
-#             if token.startswith("("):
-#                 token = token[1:]
-#                 stack.append("(")
-#             if token.endswith(")"):
-#                 token = token[:-1]
-#             if token in inverted_index:
-#                 stack.append(inverted_index[token])
-#             else:
-#                 raise ValueError(f"Invalid query: '{token}' is not a valid term")
-#     if len(stack) != 1:
-#         raise ValueError("Invalid query: too many operands or not enough operators")
-#     return stack.pop()
-
-query = "(manhattan AND new)"
-
-# #  OR (new AND york) OR NOT (big OR apple OR red OR taxi OR yellow)
-# tokens = query.split()
-# # tokens = 
-
-# # result = boolean_and(inverted_index["manhattan"], inverted_index["new"])
-# print(result)
-# process_query(query, inverted_index)
-# print(tokens)
-
-
-# print(boolean_and("manhattan", "new"))
+# ---------------------------------------------------------------------------------------------------------
 
 def process_query(query, inverted_index):
     stack = []
@@ -110,6 +60,7 @@ def process_query(query, inverted_index):
     flag=0
     for i in range(len(tokens)):
         if flag==1: continue
+
         if tokens[i] == "AND":
             operand2 = stack.pop()
             if tokens[i+1].endswith(")"):
@@ -117,13 +68,18 @@ def process_query(query, inverted_index):
             operand1 = tokens[i+1]
             flag=1
             print(operand2, operand1)
-            # result = boolean_and(operand1, operand2)
-            # stack.append(result)
-        # elif token == "OR":
-        #     operand2 = stack.pop()
-        #     operand1 = stack.pop()
-        #     result = boolean_or(operand1, operand2)
-        #     stack.append(result)
+            result = boolean_and(operand1, operand2)
+            stack.append(result)
+
+        elif tokens[i] == "OR":
+            operand2 = stack.pop()
+            print(operand2+"Hello")
+            if tokens[i+1].endswith(")"):
+                tokens[i+1] = tokens[i+1][:-1]
+            operand1 = tokens[i+1]
+            print(operand1+"HI")
+            result = boolean_or(operand1, operand2)
+            stack.append(result)
         # elif token == "NOT":
         #     operand = stack.pop()
         #     result = boolean_not(operand, num_docs=len(inverted_index["manhattan"]))
@@ -136,12 +92,12 @@ def process_query(query, inverted_index):
             if tokens[i].endswith(")"):
                 tokens[i] = tokens[i][:-1]
             if tokens[i] in inverted_index:
-                stack.append(inverted_index[tokens[i]])
+                stack.append(tokens[i])
             else:
                 raise ValueError(f"Invalid query: '{tokens[i]}' is not a valid term")
-    if len(stack) != 1:
-        raise ValueError("Invalid query: too many operands or not enough operators")
+
     return stack.pop()
 
+query = "(manhattan OR new)"
 result = process_query(query, inverted_index)
 print(result)
